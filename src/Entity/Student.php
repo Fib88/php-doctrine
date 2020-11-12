a7a122da7294d111e673cf909e6a0c5cc5940b2e
Fib88 <nsukifrank@hotmail.com>
2020-11-12 11:18:25 +0100

    recreated entities with relation looking to migrate to db

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -1,91 +1,109 @@
 <?php
-declare(strict_types = 1);
 
 namespace App\Entity;
 
-
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @Table('students')
+ * @ORM\Entity(repositoryClass=StudentRepository::class)
  */
 class Student
 {
-    /**@Id
-     * @Column(type="integer")
-     * @GeneratedValue *
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
      */
     private $id;
 
-    /** @Column(type="string") **/
-    private $firstName;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
 
-    /** @Column(type="string") **/
-    private $lastName;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
 
-    /** @Column(type="string") **/
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $email;
 
-    /** @Embedded(class="Address") */
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $address;
 
     /**
-     * One teacher has many students. This is the inverse side.
-     * @OneToMany(targetEntity="Teacher", mappedBy="teacher")
+     * @ORM\ManyToOne(targetEntity=Teacher::class, inversedBy="students")
      */
     private $teacher;
 
-    public function getId() :? int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getFirstName() : string
+    public function getFirstname(): ?string
     {
-        return $this->firstName;
+        return $this->firstname;
     }
 
-    public function setFirstName(string $firstName)
+    public function setFirstname(string $firstname): self
     {
-        $this->firstName = $firstName;
+        $this->firstname = $firstname;
+
+        return $this;
     }
 
-    public function getLastName() : string
+    public function getLastname(): ?string
     {
-        return $this->lastName;
+        return $this->lastname;
     }
 
-    public function setLastName(string $lastName)
+    public function setLastname(string $lastname): self
     {
-        $this->lastName = $lastName;
+        $this->lastname = $lastname;
+
+        return $this;
     }
 
-    public function getEmail() : string
+    public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function setEmail(string $email)
+    public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
     }
 
-    public function getTeacher() : Teacher
-    {
-        return $this->teacher;
-    }
-
-    public function setTeacher(Teacher $teacher)
-    {
-        $this->teacher = $teacher;
-    }
-
-    public function getAddress() : Address
+    public function getAddress(): ?string
     {
         return $this->address;
     }
 
-    public function setAddress(Address $address)
+    public function setAddress(string $address): self
     {
         $this->address = $address;
+
+        return $this;
+    }
+
+    public function getTeacher(): ?Teacher
+    {
+        return $this->teacher;
+    }
+
+    public function setTeacher(?Teacher $teacher): self
+    {
+        $this->teacher = $teacher;
+
+        return $this;
     }
 }
